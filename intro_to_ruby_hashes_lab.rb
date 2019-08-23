45086727b1bc812f2a12ab2885ac5b78a987b40e
Cody Dupuis <ctdupuis7@gmail.com>
2019-08-23 23:18:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
 	      :k5 => 5,
 	      :k6 => 6,
 	      :k7 => 7,
-	      :reading_railroads => {}
+	      :reading_railroad => {}
 	  },
 	 :rent_in_dollars => {
 	      :one_piece_owned => 25,
